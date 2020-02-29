173b22d771ee6df160ebe8258a67df9f0edc4510
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-02-29 11:43:06 +0100

    Make a controller

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function show()
+    {
+        return view('contact');
+    }
+
+    public function store()
+    {
+        $email = request('email');
+
+        dd($email);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', 'ContactController@show');
+Route::post('contact', 'ContactController@store');
