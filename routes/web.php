c285ba188d05a830d516b99db22416f237a0d5af
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-03-01 12:24:15 +0100

    Refactor: Add missing slash to a route

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ Route::get('/', function () {
 });
 
 Route::get('/contact', 'ContactController@show');
-Route::post('contact', 'ContactController@store');
+Route::post('/contact', 'ContactController@store');
